d981beb99cfcab80f7d950db53e3919144feac9c
Shweryl <shwezin2001shwezin2001@gmail.com>
2025-08-02 19:32:55 +0100

    sz-fix-iframe

--- src/modules/demo/components/EmbedVideo.tsx
+++ src/modules/demo/components/EmbedVideo.tsx
@@ -10,10 +10,10 @@ export default function EmbedVideo() {
                         className="w-full h-full" 
                         src={embedData.src}                         
                         title="YouTube video player" 
-                        frameborder="0" 
+                        frameBorder="0" 
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                        referrerpolicy="strict-origin-when-cross-origin" 
-                        allowfullscreen></iframe>
+                        referrerPolicy="strict-origin-when-cross-origin" 
+                        allowFullScreen></iframe>
                 </div>
             </div>
         </section>
